9b9a7e11a40761ada0ed9599c39d9ba4d803a4eb
OsipovDN <disorder_2@list.ru>
2023-02-15 16:15:44 +0300

    Доработка флаговКорректное отображение поля

--- Game/src/Player.cpp
+++ Game/src/Player.cpp
@@ -38,12 +38,11 @@ bool Player::intersecShip(int& _x, int& _y, int& _dir, int& _deck) noexcept {
 bool Player::getShot(int& _x, int& _y) {
 	bool flag = false;
 	int temp = ship_count;
-	std::for_each(navy.begin(), navy.end(), [&](Ship s)mutable {
+	std::for_each(navy.cbegin(), navy.cend(), [&](Ship s)mutable {
 		if (s.IsHit(_x, _y))
 			flag = true;
 		if (!s.Islife())
 			ship_count--;
-		}
 		});
 	std::cout << flag << " " <<temp<<" " << ship_count << std::endl;
 	if (flag) {
