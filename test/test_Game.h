e0fea45218ee9842416ba4b777d5dc57adfc1941
OsipovDN <didison88@gmail.com>
2023-03-09 14:36:12 +0300

    Новые методы в классе Player

--- test/test_Game.h
+++ test/test_Game.h
@@ -1,1 +1,23 @@
 #pragma once
+#include "Game.h"
+#include <gtest/gtest.h>
+#include <iostream>
+#include <memory>
+#include <vector>
+#include <algorithm>
+
+
+struct PlayerFixture : public testing::Test {
+	std::unique_ptr <Player> plr1;
+	std::unique_ptr <Player> plr2;
+
+
+	static void SetUpTestSuite() {}
+
+	static void TearDownTestSuite() {}
+	void SetUp() override {
+		Game();
+	}
+	void TearDown() override {}
+
+};
